c09c910738fa26ba9a8f0e75bf0649e9a37e8441
ReYueY1ng <107557918+ReYueY1ng@users.noreply.github.com>
2026-06-29 13:16:40 +0800

    fix(proxy): 子Peer断开时不透传数据给客户端(仅销毁)

--- raknet_proxy.cpp
+++ raknet_proxy.cpp
@@ -1040,14 +1040,10 @@ int main(int argc, char **argv) {
           ctx.packetsReceived++;
         } else if (id == ID_DISCONNECTION_NOTIFICATION) {
           printf("[子Peer] 目标服务器主动断开连接 UIN:%u\n", pair.first);
-          peer->Send((const char *)(cp->data), cp->length, HIGH_PRIORITY,
-                     RELIABLE_ORDERED, 0, ctx.clientAddress, false);
           DestroyClient(ctx.childPeer);
           ctx.childPeer = NULL;
         } else if (id == ID_CONNECTION_LOST) {
           printf("[子Peer] 目标服务器连接丢失 UIN:%u\n", pair.first);
-          peer->Send((const char *)(cp->data), cp->length, HIGH_PRIORITY,
-                     RELIABLE_ORDERED, 0, ctx.clientAddress, false);
           DestroyClient(ctx.childPeer);
           ctx.childPeer = NULL;
         } else {
